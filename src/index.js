610f24df08fd2286f14615bdf4093c024072ed30
Alex <alex.holder@bigfootds.com>
2024-09-03 19:30:14 +1000

    feat: project init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let defaultEnv = {
+	PORT: 3030,
+	SOME_DATABASE_URL: "blah blah blah",
+	SECURITY_TOKEN: "aosldkhalfkhasfdas",
+	SECRET_API_KEY: "01328eyu1903r82hegfsl;vkdns",
+	JWT_SECRET_KEY:"some special jwt salt word or phrase"
+}
+
+console.log(defaultEnv);
